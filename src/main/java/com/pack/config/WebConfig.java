5ae2275ec402d83afe2fe1e03808fead1f991ec1
Muhammad Fayaz <83117029+Fayaz89@users.noreply.github.com>
2024-07-26 12:11:23 +0530

    Update WebConfig.java

--- src/main/java/com/pack/config/WebConfig.java
+++ src/main/java/com/pack/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://34.71.248.96:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
